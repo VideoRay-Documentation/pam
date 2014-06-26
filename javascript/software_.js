6f2795e8fc0fa6a5c7f0b8126b57e901fe12cdf2
ChuckJKelly <kellyc4@rpi.edu>
2014-06-26 17:03:40 -0400

    Baud Rate

--- javascript/software_.js
+++ javascript/software_.js
@@ -22,12 +22,14 @@ if (document.images)
    term = new Image;
    diagh = new Image;
    comms = new Image;
+   baud = new Image;
    
    baset.src = '../images/con_diag.png';
    viewt.src = '../images/con_diag.png';
    term.src = '../images/con_diag_term.png';
    diagh.src = '../images/diag_s1.png';
    comms.src = '../images/diag_s2.png';
+   baud.src = '../images/con_baud.png';
    
    
    
